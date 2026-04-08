e9e6af86de53cee1f5871df36ccc6f2a371724c8
Bhashkar <bhashkar.dc7@gmail.com>
2026-04-08 18:50:26 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
+console.log("API SECRET:", process.env.CLOUDINARY_API_SECRET);
+
+
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js"
+import doctorRouter from "./routes/doctorRoute.js"
+import adminRouter from "./routes/adminRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
+
+app.get("/", (req, res) => {
+  res.send("API Working")
+});
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`))
