46ef0c28953af23c65348ba97e7fbf50582404b5
Bhashkar <bhashkar.dc7@gmail.com>
2026-04-09 19:35:24 +0530

    Update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto is a modern full-stack doctor appointment booking web application. It allows patients to browse doctors by speciality, view real-time availability, select time slots, book appointments, and manage their profiles. The platform is built using the MERN stack (React.js, Node.js, Express, MongoDB) with role-based access for patients, doctors, and administrators.</p>
         </div>
 
         <div>
@@ -33,7 +33,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2026 @ Prescripto.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2026 @ Prescripto-Bhashkar.com - All Right Reserved.</p>
       </div>
 
     </div>
